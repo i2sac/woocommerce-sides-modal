bb962d41cd62bdca0141422dc1f09f5e40c30a24
lodiouf <louisisaacdf@gmail.com>
2025-09-06 15:02:26 +0000

    Fix plugin

--- includes/class-wsm-admin.php
+++ includes/class-wsm-admin.php
@@ -5,28 +5,35 @@ if (!defined('ABSPATH'))
 class WSM_Admin
 {
     private $options;
-    private $menu_slug = 'wc-sides-modal-settings';
+    private $page_title = 'WooCommerce Sides Modal';
+    private $menu_title = 'Sides Modal';
+    private $menu_slug = 'woocommerce-sides-modal';
+    private $capability = 'manage_options';
 
     public function __construct()
     {
-        add_action('admin_menu', array($this, 'add_plugin_page'));
-        add_action('admin_init', array($this, 'register_settings'));
-
         $this->options = get_option(WSM_OPTIONS_KEY, array(
             'categories' => '',
             'shortcode' => '[html_block id=""]'
         ));
     }
 
-    public function add_plugin_page()
+    public function init()
     {
-        add_submenu_page(
-            'woocommerce',
-            'WooCommerce Sides Modal',
-            'Sides Modal',
-            'manage_woocommerce',
+        add_action('admin_menu', array($this, 'add_admin_menu'));
+        add_action('admin_init', array($this, 'register_settings'));
+    }
+
+    public function add_admin_menu()
+    {
+        add_menu_page(
+            $this->page_title,
+            $this->menu_title,
+            $this->capability,
             $this->menu_slug,
-            array($this, 'create_admin_page')
+            array($this, 'create_admin_page'),
+            'dashicons-feedback',
+            56
         );
     }
 
@@ -35,88 +42,74 @@ class WSM_Admin
         register_setting(
             $this->menu_slug,
             WSM_OPTIONS_KEY,
-            array(
-                'sanitize_callback' => array($this, 'sanitize')
-            )
+            array($this, 'sanitize')
         );
     }
 
     public function create_admin_page()
     {
+        if (!current_user_can($this->capability)) {
+            wp_die(__('Vous n\'avez pas les permissions suffisantes pour accéder à cette page.'));
+        }
         ?>
         <div class="wrap">
-            <h1>WooCommerce Sides Modal</h1>
-            <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
-                <?php wp_nonce_field($this->menu_slug . '_save'); ?>
-                <input type="hidden" name="action" value="save_wsm_settings">
-
-                <table class="form-table">
+            <h1><?php echo esc_html($this->page_title); ?></h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields($this->menu_slug);
+                do_settings_sections($this->menu_slug);
+                ?>
+                <table class="form-table" role="presentation">
                     <tr>
-                        <th scope="row">Catégories WooCommerce</th>
+                        <th scope="row">
+                            <label for="categories">Catégories WooCommerce</label>
+                        </th>
                         <td>
-                            <input type="text" name="<?php echo WSM_OPTIONS_KEY; ?>[categories]"
-                                value="<?php echo esc_attr($this->options['categories']); ?>" class="regular-text" />
+                            <input type="text"
+                                id="categories"
+                                name="<?php echo WSM_OPTIONS_KEY; ?>[categories]"
+                                value="<?php echo esc_attr($this->options['categories']); ?>"
+                                class="regular-text"
+                            />
                             <p class="description">
                                 Entrez les slugs des catégories séparés par des virgules (ex: senegalese-meals,african-meals)
                             </p>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">Shortcode du bloc HTML</th>
+                        <th scope="row">
+                            <label for="shortcode">Shortcode du bloc HTML</label>
+                        </th>
                         <td>
-                            <input type="text" name="<?php echo WSM_OPTIONS_KEY; ?>[shortcode]"
-                                value="<?php echo esc_attr($this->options['shortcode']); ?>" class="regular-text" />
+                            <input type="text"
+                                id="shortcode"
+                                name="<?php echo WSM_OPTIONS_KEY; ?>[shortcode]"
+                                value="<?php echo esc_attr($this->options['shortcode']); ?>"
+                                class="regular-text"
+                            />
                             <p class="description">
                                 Entrez le shortcode complet (ex: [html_block id="1234"])
                             </p>
                         </td>
                     </tr>
                 </table>
-
                 <?php submit_button(); ?>
             </form>
         </div>
         <?php
     }
 
-    public function sanitize($input)
-    {
+    public function sanitize($input) {
         $sanitized = array();
-
+        
         if (isset($input['categories'])) {
             $sanitized['categories'] = sanitize_text_field($input['categories']);
         }
-
+        
         if (isset($input['shortcode'])) {
             $sanitized['shortcode'] = sanitize_text_field($input['shortcode']);
         }
-
+        
         return $sanitized;
-    }
-
-    public function save_settings()
-    {
-        if (!current_user_can('manage_woocommerce')) {
-            wp_die(__('Vous n\'avez pas les permissions suffisantes pour accéder à cette page.'));
-        }
-
-        check_admin_referer($this->menu_slug . '_save');
-
-        $settings = isset($_POST[WSM_OPTIONS_KEY]) ? $_POST[WSM_OPTIONS_KEY] : array();
-        $settings = $this->sanitize($settings);
-
-        update_option(WSM_OPTIONS_KEY, $settings);
-
-        wp_redirect(add_query_arg(
-            'settings-updated',
-            'true',
-            admin_url('admin.php?page=' . $this->menu_slug)
-        ));
-        exit;
-    }
-
-    public function init()
-    {
-        add_action('admin_post_save_wsm_settings', array($this, 'save_settings'));
     }
 }
