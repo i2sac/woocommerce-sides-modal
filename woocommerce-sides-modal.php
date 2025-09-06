be347dc9f589dec931ef346c368085af4f250563
lodiouf <louisisaacdf@gmail.com>
2025-09-06 13:28:40 +0000

    Add 'Settings' link

--- woocommerce-sides-modal.php
+++ woocommerce-sides-modal.php
@@ -18,6 +18,13 @@ define('WSM_VERSION', '1.0.0');
 define('WSM_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WSM_PLUGIN_URL', plugin_dir_url(__FILE__));
 
+// Ajout du lien Settings dans la liste des plugins
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), function($links) {
+    $settings_link = '<a href="' . admin_url('options-general.php?page=wc-sides-modal') . '">Réglages</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+});
+
 // Vérification de WooCommerce
 function wsm_check_woocommerce()
 {
