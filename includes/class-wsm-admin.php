5b7725c1faf9db731db3befe8954ce9855e6c6f3
lodiouf <louisisaacdf@gmail.com>
2025-09-06 14:50:58 +0000

    Fix plugin

--- includes/class-wsm-admin.php
+++ includes/class-wsm-admin.php
@@ -5,36 +5,77 @@ if (!defined('ABSPATH'))
 class WSM_Admin
 {
     private $options;
-    private $page = 'wc-sides-modal';
-    private $option_group = 'wc_sides_modal_options';
 
     public function __construct()
     {
         add_action('admin_menu', array($this, 'add_plugin_page'));
-        add_action('admin_init', array($this, 'page_init'));
-        $this->options = get_option(WSM_OPTIONS_KEY);
+        add_action('admin_init', array($this, 'init_settings'));
     }
 
     public function add_plugin_page()
     {
-        add_options_page(
-            'WooCommerce Sides Modal',
-            'Sides Modal',
-            'manage_options',
-            $this->page,
-            array($this, 'create_admin_page')
+        // Ajout de la page dans le menu principal de WordPress
+        add_menu_page(
+            'WooCommerce Sides Modal', // Titre de la page
+            'Sides Modal',             // Titre du menu
+            'manage_options',          // Capacité requise
+            'wc-sides-modal',         // Slug du menu
+            array($this, 'create_admin_page'), // Fonction de callback
+            'dashicons-cart',         // Icône
+            56                        // Position
         );
+    }
+
+    public function init_settings()
+    {
+        register_setting(
+            'wc_sides_modal_options', // Option group
+            WSM_OPTIONS_KEY,          // Option name
+            array($this, 'sanitize')  // Sanitize callback
+        );
+
+        add_settings_section(
+            'wc_sides_modal_section', // ID
+            'Paramètres',            // Title
+            null,                    // Callback
+            'wc-sides-modal'         // Page
+        );
+
+        add_settings_field(
+            'categories',             // ID
+            'Catégories WooCommerce', // Title
+            array($this, 'categories_callback'), // Callback
+            'wc-sides-modal',        // Page
+            'wc_sides_modal_section' // Section
+        );
+
+        add_settings_field(
+            'shortcode',              // ID
+            'Shortcode du bloc HTML', // Title
+            array($this, 'shortcode_callback'), // Callback
+            'wc-sides-modal',        // Page
+            'wc_sides_modal_section' // Section
+        );
+
+        $this->options = get_option(WSM_OPTIONS_KEY, array(
+            'categories' => '',
+            'shortcode' => '[html_block id=""]'
+        ));
     }
 
     public function create_admin_page()
     {
+        // Vérification des droits d'accès
+        if (!current_user_can('manage_options')) {
+            wp_die(__('Vous n\'avez pas les permissions suffisantes pour accéder à cette page.'));
+        }
         ?>
         <div class="wrap">
-            <h1>WooCommerce Sides Modal</h1>
+            <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
             <form method="post" action="options.php">
                 <?php
-                settings_fields($this->option_group);
-                do_settings_sections($this->page);
+                settings_fields('wc_sides_modal_options');
+                do_settings_sections('wc-sides-modal');
                 submit_button();
                 ?>
             </form>
@@ -42,57 +83,25 @@ class WSM_Admin
         <?php
     }
 
-    public function page_init()
-    {
-        register_setting(
-            $this->option_group,
-            WSM_OPTIONS_KEY,
-            array($this, 'sanitize')
-        );
-
-        add_settings_section(
-            'wsm_setting_section',
-            'Paramètres',
-            null,
-            $this->page
-        );
-
-        add_settings_field(
-            'categories',
-            'Catégories WooCommerce',
-            array($this, 'categories_callback'),
-            $this->page,
-            'wsm_setting_section'
-        );
-
-        add_settings_field(
-            'shortcode',
-            'Shortcode du bloc HTML',
-            array($this, 'shortcode_callback'),
-            $this->page,
-            'wsm_setting_section'
-        );
-    }
-
     public function sanitize($input)
     {
-        $new_input = array();
+        $sanitized_input = array();
 
         if (isset($input['categories'])) {
-            $new_input['categories'] = sanitize_text_field($input['categories']);
+            $sanitized_input['categories'] = sanitize_text_field($input['categories']);
         }
 
         if (isset($input['shortcode'])) {
-            $new_input['shortcode'] = sanitize_text_field($input['shortcode']);
+            $sanitized_input['shortcode'] = sanitize_text_field($input['shortcode']);
         }
 
-        return $new_input;
+        return $sanitized_input;
     }
 
     public function categories_callback()
     {
         printf(
-            '<input type="text" id="categories" name="%s[categories]" value="%s" class="regular-text" />',
+            '<input type="text" id="categories" name="%s[categories]" value="%s" class="regular-text">',
             WSM_OPTIONS_KEY,
             esc_attr($this->options['categories'])
         );
@@ -102,7 +111,7 @@ class WSM_Admin
     public function shortcode_callback()
     {
         printf(
-            '<input type="text" id="shortcode" name="%s[shortcode]" value="%s" class="regular-text" />',
+            '<input type="text" id="shortcode" name="%s[shortcode]" value="%s" class="regular-text">',
             WSM_OPTIONS_KEY,
             esc_attr($this->options['shortcode'])
         );
